77710cae1bc729696388693837357c1f71eea0fa
Joshua Bloom <96476058+AlgoBloom@users.noreply.github.com>
2023-02-03 18:55:50 -0700

    Added check for name.

--- test/Token.test.js
+++ test/Token.test.js
@@ -1,9 +1,17 @@
 const Token = artifacts.require("./Token")
 
+require('chai')
+    .use(require('chai-as-promised'))
+    .should()
+
 contract('Token', (accounts) => {
     describe('deployment', () => {
-        it('tracks the name', () => {
-            await Token.new()
+        it('tracks the name', async () => {
+            // fetch token from the blockchain
+            const token = await Token.new()
+            const result = token.name()
+            // check the name
+            result.should.equal('MyName')
         })
     }
 })
