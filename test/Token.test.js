446d8e3ce422f6b028f57e215ed4ae69c0643c23
Joshua Bloom <96476058+AlgoBloom@users.noreply.github.com>
2023-02-03 20:11:33 -0700

    Tests working.

--- test/Token.test.js
+++ test/Token.test.js
@@ -5,40 +5,40 @@ require('chai')
     .should()
 
 contract('Token', (accounts) => {
-    const name = 'My Name'
-    const symbol = 'Symbol'
-    const decimals = 'Decimals'
-    const totalSupply = 'Total Supply'
+    const name = 'DApp Token'
+    const symbol = 'DAPP'
+    const decimals = '18'
+    const totalSupply = '1000000000000000000000000'
     let token
 
+    beforeEach(async () => {
+        // fetch token from the blockchain
+        token = await Token.new()
+    })
+
     describe('deployment', () => {
-        beforeEach(async () => {
-            // fetch token from the blockchain
-            const token = await Token.new()
-        })
+            it('tracks the name', async () => {
+                const result = await token.name()
+                // check the name
+                result.should.equal(name)
+            })
 
-        it('tracks the name', async () => {
-            const result = await token.name()
-            // check the name
-            result.should.equal(name)
-        })
+            it('tracks the symbol', async () => {
+                const result = await token.symbol()
+                // check the symbol
+                result.should.equal(symbol)
+            })
 
-        it('tracks the symbol', async () => {
-            const result = await token.symbol()
-            // check the symbol
-            result.should.equal(symbol)
-        })
+            it('tracks the decimals', async () => {
+                const result = await token.decimals()
+                // check the decimals
+                result.toString().should.equal(decimals)
+            })
 
-        it('tracks the decimals', async () => {
-            const result = await token.decimals()
-            // check the decimals
-            result.should.equal(decimals)
-        })
-
-        it('tracks the total supply', async () => {
-            const result = await token.totalSupply()
-            // check the total supply
-            result.should.equal(totalSupply)
-        })
+            it('tracks the total supply', async () => {
+                const result = await token.totalSupply()
+                // check the total supply
+                result.toString().should.equal(totalSupply)
+            })
     })
 })
